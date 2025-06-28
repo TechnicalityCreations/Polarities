621026f1777c6739784f298c55e60becc7d1b269
bubbyboytoo <mbkirbydude@gmail.com>
2025-06-28 17:18:00 -0300

    shroom visor lil fixes + localization

--- Content/Items/Armor/Ranged/Hardmode/Pieces/ShroomiteDartHelmet.cs
+++ Content/Items/Armor/Ranged/Hardmode/Pieces/ShroomiteDartHelmet.cs
@@ -17,8 +17,10 @@ namespace Polarities.Content.Items.Armor.Ranged.Hardmode.Pieces
 
         public override void SetDefaults()
         {
-            Item.CloneDefaults(ItemID.ShroomiteHelmet);
+            Item.CloneDefaults(ItemID.AaronsHelmet);
             Item.headSlot = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Head);
+			Item.rare = ItemRarityID.Yellow;
+			Item.value = Item.buyPrice(gold: 5);
         }
     }
 
@@ -29,12 +31,14 @@ namespace Polarities.Content.Items.Armor.Ranged.Hardmode.Pieces
         public override void ModifyActiveShop(NPC npc, string shopName, Item[] items)
         {
             if (npc.type == NPCID.Truffle &&
-                (ModUtils.Contains(Main.LocalPlayer.inventory, ItemID.ShroomiteHeadgear
-              || ModUtils.Contains(Main.LocalPlayer.inventory, ItemID.ShroomiteMask
-              || ModUtils.Contains(Main.LocalPlayer.inventory, ItemID.ShroomiteHelmet
-              || ModUtils.Contains(Main.LocalPlayer.armor, ItemID.ShroomiteHeadgear
-              || ModUtils.Contains(Main.LocalPlayer.armor, ItemID.ShroomiteMask
-              || ModUtils.Contains(Main.LocalPlayer.armor, ItemID.ShroomiteHelmet))
+                (ModUtils.Contains(Main.LocalPlayer.inventory, ItemID.ShroomiteHeadgear)
+              || ModUtils.Contains(Main.LocalPlayer.inventory, ItemID.ShroomiteMask)
+              || ModUtils.Contains(Main.LocalPlayer.inventory, ItemID.ShroomiteHelmet)
+              || ModUtils.Contains(Main.LocalPlayer.armor, ItemID.ShroomiteHeadgear)
+              || ModUtils.Contains(Main.LocalPlayer.armor, ItemID.ShroomiteMask)
+              || ModUtils.Contains(Main.LocalPlayer.armor, ItemID.ShroomiteHelmet)
+				)
+			)
             {
                 int firstFreeIndex = items.Length - 1;
                 for (int i = items.Length - 1; i > -1; i--)
