577bbfdb13a25690c6c9440fa76d02a9a03f46cb
goose <40964328+TDLPoof@users.noreply.github.com>
2025-06-28 14:23:56 -0500

    Convert Shroomite Visor to vanity

--- Content/Items/Armor/Ranged/Hardmode/Pieces/ShroomiteDartHelmet.cs
+++ Content/Items/Armor/Ranged/Hardmode/Pieces/ShroomiteDartHelmet.cs
@@ -19,32 +19,6 @@ namespace Polarities.Content.Items.Armor.Ranged.Hardmode.Pieces
             Item.CloneDefaults(ItemID.ShroomiteHelmet);
             Item.headSlot = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Head);
         }
-
-        public override void UpdateEquip(Player player)
-        {
-            player.GetCritChance(DamageClass.Ranged) += 5f;
-            player.GetModPlayer<PolaritiesPlayer>().dartDamage += 0.15f;
-        }
-
-        public override bool IsArmorSet(Item head, Item body, Item legs)
-        {
-            return body.type == ItemID.ShroomiteBreastplate && legs.type == ItemID.ShroomiteLeggings;
-        }
-
-        public override void UpdateArmorSet(Player player)
-        {
-            player.setBonus = Language.GetTextValue("ArmorSetBonus.Shroomite");
-            player.shroomiteStealth = true;
-        }
-
-        public override void AddRecipes()
-        {
-            CreateRecipe()
-                .AddIngredient(ItemID.ShroomiteBar, 12)
-                .AddTile(TileID.MythrilAnvil)
-                .SortAfterFirstRecipesOf(ItemID.ShroomiteHelmet)
-                .Register();
-        }
     }
 }
 
