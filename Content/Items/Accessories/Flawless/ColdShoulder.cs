600dfb53687b4a94d0419ff27f46f51365b18e0f
goose <40964328+TDLPoof@users.noreply.github.com>
2025-08-24 15:48:07 -0500

    Cold Shoulder rework (Programmer art version)
    TODO: Make some real art!

--- Content/Items/Accessories/Flawless/ColdShoulder.cs
+++ Content/Items/Accessories/Flawless/ColdShoulder.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Polarities.Global;
+using Terraria.Audio;
+using Terraria.GameContent;
 using static Terraria.ModLoader.ModContent;
 
 namespace Polarities.Content.Items.Accessories.Flawless
 {
+    
     public class ColdShoulder : ModItem
     {
         public override void SetStaticDefaults()
@@ -20,10 +24,10 @@ namespace Polarities.Content.Items.Accessories.Flawless
         public override void SetDefaults()
         {
             Item.noUseGraphic = true;
-            Item.damage = 0;
+            Item.damage = 50;
             Item.knockBack = 7f;
             Item.useStyle = ItemUseStyleID.Shoot;
-            Item.shootSpeed = 13f;
+            Item.shootSpeed = 18f;
             Item.shoot = ProjectileType<ColdShoulderProjectile>();
             Item.width = 30;
             Item.height = 26;
@@ -35,11 +39,15 @@ namespace Polarities.Content.Items.Accessories.Flawless
             Item.value = 5000;
         }
     }
-
     public class ColdShoulderProjectile : ModProjectile
     {
         //texture cacheing
         public static Asset<Texture2D> ChainTexture;
+
+        public NPC attached;
+        public int attachedTime = -1;
+        
+        private SpriteEffects effects = SpriteEffects.None;
 
         public override void Load()
         {
@@ -55,25 +63,58 @@ namespace Polarities.Content.Items.Accessories.Flawless
         public override void SetDefaults()
         {
             Projectile.netImportant = true;
-            Projectile.width = 26;
-            Projectile.height = 26;
+            Projectile.width = 24;
+            Projectile.height = 32;
 
             Projectile.aiStyle = 7;
             Projectile.friendly = true;
             Projectile.penetrate = -1;
             Projectile.tileCollide = false;
             Projectile.timeLeft *= 10;
+            
         }
+
+        private int pullTime = 0;
 
         public override void AI()
         {
-            foreach (NPC npc in Main.npc)
+            Player player = Main.player[Projectile.owner];
+            
+            if (Projectile.localAI[0] == 0)
             {
-                if (!npc.active) continue;
-                if (npc.Distance(Projectile.position) > 160) continue;
+                effects = Projectile.velocity.X > 0
+                    ? SpriteEffects.FlipHorizontally
+                    : SpriteEffects.None;
+                Projectile.localAI[0] = 1;
+            }
+            if (!Main.dedServ)
+            {
+                Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.IceTorch).noGravity = true;
+            }
+            if (attached != null && attachedTime > 0)
+            {
+                Projectile.Center = attached.Center;
+                attachedTime--;
+                if (attached.life < 1) attachedTime = -1;
+                if (!attached.boss) attached.position -= attached.velocity * Math.Min(1, attached.knockBackResist);
+            }
+            else if (attached != null && attached.life > 0 && pullTime < 5)
+            {
+                Projectile.friendly = true;
+                if (attached.knockBackResist < 0.2f || attached.boss) player.velocity += player.Center.DirectionTo(Projectile.Center) * 5f;
+                else attached.velocity += attached.Center.DirectionTo(player.Center) * 5f * Math.Min(1, attached.knockBackResist);
+                pullTime++;
+            }
+        }
 
-                float freezeScale = (160 - npc.Distance(Projectile.position)) / 160f;
-                npc.position -= npc.velocity * freezeScale * npc.knockBackResist;
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (attached == null)
+            {
+                SoundEngine.PlaySound(SoundID.Item27, target.Center);
+                attached = target;
+                attachedTime = 60;
+                Projectile.friendly = false;
             }
         }
 
@@ -81,6 +122,11 @@ namespace Polarities.Content.Items.Accessories.Flawless
         public override float GrappleRange()
         {
             return 500f;
+        }
+
+        public override bool? CanUseGrapple(Player player)
+        {
+            return player.ownedProjectileCounts[Type] < 2;
         }
 
         public override void NumGrappleHooks(Player player, ref int numHooks)
@@ -96,7 +142,7 @@ namespace Polarities.Content.Items.Accessories.Flawless
 
         public override void GrapplePullSpeed(Player player, ref float speed)
         {
-            speed = 20;
+            speed = 10f;
         }
 
         public override bool PreDrawExtras()
@@ -119,6 +165,15 @@ namespace Polarities.Content.Items.Accessories.Flawless
                     new Rectangle(0, 0, 10, 16), Color.White, projRotation,
                     new Vector2(10 * 0.5f, 16 * 0.5f), 1f, SpriteEffects.None, 0f);
             }
+            return false;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = TextureAssets.Projectile[Type].Value;
+            Main.spriteBatch.Draw(texture, new Vector2(Projectile.Center.X - Main.screenPosition.X, Projectile.Center.Y - Main.screenPosition.Y),
+                new Rectangle(0, 0, 24, 32), Color.White, Projectile.rotation,
+                new Vector2(texture.Width * 0.5f, texture.Height * 0.5f), 1f, effects, 0f);
             return false;
         }
     }
